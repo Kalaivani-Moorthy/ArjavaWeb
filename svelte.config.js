5e15f6e332340f79f7fdb2cf86ddb4901030077c
Kalaivani-Moorthy <kalai.arjava@gmail.com>
2025-02-21 12:51:47 +0530

    json changed

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,17 @@
-import adapter from '@sveltejs/adapter-auto';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapter from '@sveltejs/adapter-static';
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
-	}
+export default {
+  kit: {
+    adapter: adapter({
+      pages: 'build',
+      assets: 'build',
+      fallback: '200.html', // For SPA routing
+      precompress: false,
+    }),
+    paths: {
+      base: process.env.NODE_ENV === 'production' ? '/ArjavaWeb' : '', // Adjust to your repo name
+    },
+  },
+  preprocess: vitePreprocess(),
 };
-
-export default config;
